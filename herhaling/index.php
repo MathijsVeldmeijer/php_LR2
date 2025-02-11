b46f33664a22dd0d069852778ccf7d2602034242
MathijsVeldmeijer <144016735+MathijsVeldmeijer@users.noreply.github.com>
2025-02-11 14:16:23 +0100

    1.1 af

---
+++ herhaling/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+
+$firstName = "Jan";
+$age = 25;
+$height = 1.75;
+$isStudent = true;
+
+echo"voornaam $firstName <br>";
+echo"leeftijd $age <br>";
+echo"lengte $height <br>";
+echo"is student $isStudent <br>";
+
+?>
+
+</body>
+</html>
