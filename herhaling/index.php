09341059e4e62148bfeaa6204ea009fff91d0475
MathijsVeldmeijer <144016735+MathijsVeldmeijer@users.noreply.github.com>
2025-02-11 15:07:52 +0100

    1.1 t/m 1.4 af

--- herhaling/index.php
+++ herhaling/index.php
@@ -9,6 +9,8 @@
 
 <?php
 
+// 1.1
+
 $firstName = "Jan";
 $age = 25;
 $height = 1.75;
@@ -17,7 +19,51 @@ $isStudent = true;
 echo"voornaam $firstName <br>";
 echo"leeftijd $age <br>";
 echo"lengte $height <br>";
-echo"is student $isStudent <br>";
+echo"is student $isStudent <br><br>";
+
+
+//1.2
+
+// $3RandomNumbers = 395;
+// $@mail = "wasd@yahoo.com";
+// $echo = echo;
+// $firstname = "Mathijs";
+// $achternaam = "Veldmeijer";
+
+$randomNumbers3 = 395;
+$mail = "wasd@yahoo.com";
+$reverbaration = "echo";
+$firstName = "Mathijs"; //hoofdletter N
+$lastName = "Veldmeijer";
+
+
+// 1.3
+
+$city = "Amsterdam";
+$population = 821752;
+$averageTemperature  = 17.5;
+$isCapital = true;
+
+echo"$city <br>";
+echo"$population <br>";
+echo"$averageTemperature <br>";
+echo"$isCapital <br><br>";
+
+
+// 1.4
+
+$a = 10; 
+$b = 3.14;
+$c = "PHP";
+
+
+$a = "Tien";
+var_dump($a);
+$b = 3;
+var_dump($b);
+$c = false;
+var_dump($c);
+
 
 ?>
 
